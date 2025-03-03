e8ed468844766145001aed55b2808548199645ef
andrewbrayshaw <andrew.brayshaw2@gmail.com>
2025-03-03 17:46:44 +1100

    Update app.js
    Removed app.listen localhost to export default app for vercel to handle serverless request

--- app.js
+++ app.js
@@ -68,41 +68,12 @@ app.get('/CoreLogic/:address', async (request, response) => {
     console.error("Error fetching data:", error);
     return response.status(500).json({ message: "Internal Server Error" });
   }
-})
-  
-//app.get('/ArcGIS/:address', async(request, response) => {
-//  const address = request.params.address
-//  const api_url = ``
-//  const fetch_response = await fetch(api_url);
-//  const json = await fetch_response.json()
-//  //console.log(json)
-//  const xmin = json.candidates[0].extent.xmin
-//  const xmax = json.candidates[0].extent.xmax
-//  const ymin = json.candidates[0].extent.ymin
-//  const ymax = json.candidates[0].extent.ymax
-//  const api_url2 = `https://maps.six.nsw.gov.au/arcgis/rest/services/public/Valuation/MapServer/5/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22xmin%22%3A${xmin}%2C%22ymin%22%3A${ymin}%2C%22xmax%22%3A${xmax}%2C%22ymax%22%3A${ymax}%2CspatialReference%3A%7Bwkid%3A102100%7D%7D&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100`
-//  const fetch_response2 = await fetch(api_url2);
-//  const json2 = await fetch_response2.json()
-//  //console.log(json2)
-//
-//  //for (var i=0; i<json2.features.length; i++){
-//  //  if (json2.features[i].attributes.address.replace(/\s*$/,"") == address){
-//  //    console.log(json2.features[i].attributes)
-//  //    response.json(json2.features[i].attributes)
-//  //    console.log("Land Value:"+json2.features[i].attributes.val1_lv+" as at "+json2.features[i].attributes.val1_bd)
-//  //  }
-//  //response.json(json2)
+});
+
+// Export your app to be used by Vercel
+export default app;  
+
+//app.listen(3000, () => {
+//  console.log("Listening on port 3000")
 //})
-//app.get('/RayWhite/:address', async(request, response) => {
-//  const address = request.params.address
-//  const api_url = `https://home.raywhite.com/api/v2/address/search?q=${address}`;
-//  const fetch_response = await fetch(api_url);
-//  const json = await fetch_response.json();
-//  //console.log(json.data)
-//  response.json(json)
-//
-//})
-app.listen(3000, () => {
-  console.log("Listening on port 3000")
-})
 
